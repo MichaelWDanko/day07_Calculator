653754d52ea806f8d3b014f5c22d86e86d02f8ed
Michael Danko <MichaelDanko@icloud.com>
2016-03-08 14:06:17 -0500

    HTML and SCSS is finished, beginning to work on .JS

---
+++ js/app.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', function () {
+    var add = "+";
+    var subtract = "-";
+    var multiply = "*";
+    var divide = "%";
+
+    var firstNumber = document.getElementById('firstnumber');
+    var secondNumber = document.getElementById('secondnumber');
+})
